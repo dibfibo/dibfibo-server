36c363b0b65ea10fc83a1fe55fde7edf15bd4d5c
dibfibo <dibfibo@gmail.com>
2023-12-30 13:55:15 +0100

    functional middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './logger/logger.middleware';
 import { CatsController } from './cats/cats.controller';
+import { logger } from './logger/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware)
+    consumer.apply(logger)
     .forRoutes(CatsController)
   }
 }
